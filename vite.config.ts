4c1a59f835a1fe23bb064545a2991a58ee812868
vivodrinkfanta <vivodrinkfanta@gmail.com>
2026-08-10 17:14:26 +0200

    Support Cloudflare and GitHub Pages builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/Physics_command_center/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github-pages' ? '/Physics_command_center/' : '/',
   plugins: [react()],
-})
+}))
